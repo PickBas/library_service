2e837f026a2bae43eec069283893e6e7b26b0b13
PickBas <sayed.kirill@gmail.com>
2020-09-12 18:33:52 +0300

    Fixed pylint output

--- ci_tools/pylint/pylint_runner.sh
+++ ci_tools/pylint/pylint_runner.sh
@@ -1,4 +1,4 @@
-pylint $(python ./ci_tools/pylint/pylint_getting_files.py) --load-plugins pylint_django --disable=F0401,R0901 > ./ci_tools/pylint/file.txt
+pylint $(python ./ci_tools/pylint/pylint_getting_files.py) --load-plugins pylint_django --disable=F0401,R0901 | tee ./ci_tools/pylint/file.txt
 python ./ci_tools/pylint/pylint_get_score.py
 
 exit $?
