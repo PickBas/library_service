bfdff36547391eb3b24c4bdb4015dc3c51a69a68
PickBas <sayed.kirill@gmail.com>
2020-09-12 18:02:52 +0300

    Added index page

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,7 @@
+from django.urls import path
+
+from core.views import IndexPageView
 
 urlpatterns = [
-
+    path('', IndexPageView.as_view())
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,24 @@
+"""Core views.py"""
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class IndexPageView(View):
+    """IndexPageView class"""
+
+    def __init__(self, **kwargs: dict):
+        self.template_name = 'core/index.html'
+        super().__init__(**kwargs)
+
+    def get(self, request: HttpRequest) -> render:
+        """
+        Processing get request.
+
+        Args:
+            request: HttpRequest
+
+        Returns:
+            render
+        """
+        return render(request, self.template_name)
