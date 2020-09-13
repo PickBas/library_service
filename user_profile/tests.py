bdd53609d5afe19ae00c99218ad9ad98cec9892c
PickBas <sayed.kirill@gmail.com>
2020-09-13 15:29:22 +0300

    Added migrations into gitignore

--- user_profile/tests.py
+++ user_profile/tests.py
@@ -1,1 +1,15 @@
 """user_profile tests.py"""
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class LoginTest(TestCase):
+    """LoginTest class"""
+
+    def setUp(self) -> None:
+        self.client = Client()
+
+    def test_page_loads(self) -> None:
+        """Testing if login page loads"""
+        response = self.client.get(reverse("account_login"))
+        self.assertEquals(200, response.status_code)
