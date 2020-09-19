d2300b346a21c150a9fac306df5f474cbd4bb746
PickBas <sayed.kirill@gmail.com>
2020-09-19 14:39:59 +0300

    Added first and last names on signup

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -1,7 +1,6 @@
 """user_profile forms.py"""
 from allauth.account.forms import SignupForm
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpRequest
 
@@ -56,7 +55,28 @@ class CustomSignupForm(SignupForm):
 
     def __init__(self, *args: tuple, **kwargs: dict):
         super().__init__(*args, **kwargs)
-        self.fields['invite_key'] = forms.CharField(max_length=15, required=False)
+        self.fields['invite_key'] = forms.CharField(max_length=15,
+                                                    required=False,
+                                                    label='Секретный ключ')
+        self.fields['first_name'] = forms.CharField(max_length=50,
+                                                    required=True,
+                                                    label='Имя',
+                                                    widget=forms.TextInput(
+                                                        attrs={'placeholder': 'First name'}
+                                                    ))
+        self.fields['last_name'] = forms.CharField(max_length=50,
+                                                   required=True,
+                                                   label='Фамилия',
+                                                   widget=forms.TextInput(
+                                                       attrs={'placeholder': 'Last name'}
+                                                   ))
+        self.fields['username'].label = 'Имя пользователя'
+        self.fields['password1'].label = 'Пароль'
+        self.fields['password2'].label = 'Пароль (еще раз)'
+
+        self.fields.move_to_end('password1')
+        self.fields.move_to_end('password2')
+        self.fields.move_to_end('invite_key')
 
     def save(self, request: HttpRequest) -> None:
         """
@@ -66,8 +86,15 @@ class CustomSignupForm(SignupForm):
         """
 
         invite_key = self.cleaned_data.get('invite_key')
+        first_name = self.cleaned_data.get('first_name')
+        last_name = self.cleaned_data.get('last_name')
 
         user = super().save(request)
+
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+
         profile = Profile(user=user)
 
         if invite_key == settings.ADMIN_KEY:
