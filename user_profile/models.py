3788d2308cdc9a5fb1fa139fc03181a0492e0c55
PickBas <sayed.kirill@gmail.com>
2020-09-19 14:50:05 +0300

    Added role displaying

--- user_profile/models.py
+++ user_profile/models.py
@@ -47,3 +47,16 @@ class Profile(models.Model):
             return self.user.first_name + ' (' + self.user.username + ')'
 
         return self.user.last_name + ' (' + self.user.username + ')'
+
+    def get_role_str(self) -> str:
+        """
+        Getting profile's role
+
+        :returns: role (str)
+        """
+
+        if self.is_sys_admin:
+            return 'Администатор'
+        if self.is_librarian:
+            return 'Библиотекарь'
+        return 'Ученик'
